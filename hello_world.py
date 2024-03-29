da977cdc049354ba84d9a157788d7b6ade831384
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-03-29 11:41:47 +0100

    Factor out function into module

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+import greetings
 import sys
 
 arg_parser = argparse.ArgumentParser('Hello world app')
 arg_parser.add_argument('name', help='name to say hello to')
 options = arg_parser.parse_args()
-print(f'hello {options.name}!')
+greetings.hello()
