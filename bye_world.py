f39a3adbdda53be1d693c3b5bb0c0c406f4eb6f3
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-03-29 11:50:28 +0100

    Add bye function

--- bye_world.py
+++ bye_world.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+import greetings
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('name', help='whom to say bye to')
 options = arg_parser.parse_args()
 
-print(f'bye {options.name}!')
+greetings.bye(option.name)
