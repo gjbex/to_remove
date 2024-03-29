4b17dfc8a8677628c23dd36b96d4a36473e95148
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-03-29 11:29:49 +0100

    Add command line interface with argparse

--- bye_world.py
+++ bye_world.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 
-print('bye world!')
+import argparse
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('name', help='whom to say bye to')
+options = arg_parser.parse_args()
+
+print(f'bye {options.name}!')
